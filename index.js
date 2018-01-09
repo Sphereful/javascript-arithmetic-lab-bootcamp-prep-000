468e619664dfe8046ed279751bd88db96b5a2725
nurenberg tukie <unsuspiciousguy@gmail.com>
2018-01-09 20:17:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,32 +1,5 @@
-function add(a, b){
-  return a + b
+function log() {
+  console.log(greeting);
+  var greeting = 'Hello!';
 }
-
-function subtract(a, b){
-  return a - b
-}
-
-function multiply(a, b){
-  return a * b
-}
-
-function divide(a, b){
-  return a/b
-}
-function inc(n){
-  n++
-  return n
-}
-
-function dec(n){
-  n--
-  return n
-}
-
-function makeInt(n){
-  return parseInt(n, 10)
-}
-
-function preserveDecimal(n){
-  return parseFloat(n)
-}
+log();
